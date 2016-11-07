4bf9fe5d6416f42b4b06a86471680a7552fd560a
Iuliu Vișovan <thezingcollection2@gmail.com>
2016-11-07 09:17:46 +0200

    added us flag

--- public/js/app.js
+++ public/js/app.js
@@ -354,7 +354,7 @@ avModule
                 return 'it';
             if (city.indexOf('turk') > -1)
                 return 'tr';
-            if (city.toLower().indexOf('u.s.a') > -1 || city.toLower().indexOf('s.u.a') > -1) || city.toLower().indexOf('united') > -1))
+            if (city.toLower().indexOf('u.s') > -1 || city.toLower().indexOf('s.u') > -1 || city.toLower().indexOf('united') > -1)
                 return 'us';
         }
 
